bae2ca2057e4a0c3ed364e0497e1ea5c26f84c86
Domasch <magesthomas@gmail.com>
2023-10-27 13:10:26 +0200

    pad to power of 2 length correctly

--- intmul/intmul.c
+++ intmul/intmul.c
@@ -8,54 +8,52 @@
  * Reads input line character for character into char*. Pre-allocates 1024 and dynamically increases if growing is necessary.
  * @return char* The string that is allocated and read from the input
  */
-static char* read_line(void);
+static char *read_line(void);
+
 /**
  * Loops through the whole String and checks if each character is a HEX digit.
  * if not returns false;
  * @param str pointer to char* where the content is immutable
  * @return true or false depending on if the whole char* is a hexdigit or not
  */
-static bool is_hex(const char*);
+static bool is_hex(const char *);
 
 /**
  * Pads the new string with leading 0s up to length new_l
  * @pre new_l needs to be longer than strlen(str)
- * @param str input string to pad
+ * @param str pointer to input string
  * @param new_l new length to be padded
  */
-static int pad_string(char** , int);
+static int pad_string(char **, size_t);
 
-static int pad_string(char **pstr, int new_l) {
-    if (pstr == NULL || *pstr == NULL || new_l <= strlen(*pstr)) {
+static int pad_string(char ** input_str, size_t new_l) {
+    if (input_str == NULL || *input_str == NULL || new_l <= strlen(*input_str)) {
         return 1;
     }
 
-    int original_len = strlen(*pstr);
-    int padding_len = new_l - original_len;
-    char *new_str = realloc(*pstr, new_l + 1); // +1 for null terminator
+    size_t original_len = strlen(*input_str);
+    size_t padding_len = new_l - original_len;
+    *input_str = realloc(*input_str, new_l + 1); // +1 for null terminator
 
-    if (!new_str) {
+    if (!input_str) {
         fprintf(stderr, "Reallocation for string failed\n");
-        // No need to free *pstr; original memory remains intact upon realloc failure.
         return 1;
     }
-
-    *pstr = new_str;  // Update the input string pointer
 
     // Move the characters to the right to create space for padding
     for (int i = original_len; i >= 0; i--) {  // including null terminator
-        (*pstr)[i + padding_len] = (*pstr)[i];
+        (*input_str)[i + padding_len] = (*input_str)[i];
     }
 
     // Fill the new space with zeros
     for (int i = 0; i < padding_len; i++) {
-        (*pstr)[i] = '0';
+        (*input_str)[i] = '0';
     }
     return 0;
 }
 
 
-static bool is_hex(const char* str) {
+static bool is_hex(const char *str) {
     while (*str) {
         if (!isdigit(*str) && !(tolower(*str) >= 'a' && tolower(*str) <= 'f')) {
             return false;
@@ -65,7 +63,7 @@ static bool is_hex(const char* str) {
     return true;
 }
 
-static char* read_line(void) {
+static char *read_line(void) {
     int buffer_size = 1024;  // Initial buffer size
     int position = 0;
     char *buffer = malloc(sizeof(char) * buffer_size);
@@ -112,8 +110,21 @@ int main(void) {
         return EXIT_FAILURE;
     }
 
-    int len1 = strlen(line1);
-    int len2 = strlen(line2);
+    size_t len1 = strlen(line1);
+    size_t len2 = strlen(line2);
+
+    size_t len1Missing = len1 % 2;
+    size_t len2Missing = len2 % 2;
+
+    if (pad_string(&line1, len1+len1Missing) == 1 || pad_string(&line2, len2+len2Missing) == 1) {
+        fprintf(stderr, "Padding failed\n");
+        free(line1);
+        free(line2);
+        return EXIT_FAILURE;
+    }
+
+    len1 = strlen(line1);
+    len2 = strlen(line2);
 
     if (len1 > len2) {
         if (pad_string(&line2, len1) == 1) {
@@ -123,7 +134,7 @@ int main(void) {
             return EXIT_FAILURE;
         }
     } else if (len2 > len1) {
-        if (pad_string(&line1, len2)==1 ) {
+        if (pad_string(&line1, len2) == 1) {
             fprintf(stderr, "Padding string failed for string2\n");
             free(line1);
             free(line2);
